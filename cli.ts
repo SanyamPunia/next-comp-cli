1f725d343c9fe676b51730dbd06577e026bee5be
SanyamPunia <lewarends@gmail.com>
2024-10-31 22:05:31 +0530

    fix: :bug: update version

--- cli.ts
+++ cli.ts
@@ -5,7 +5,7 @@ import { generateComponent } from "./generate-component";
 
 async function run() {
   program
-    .version("1.0.4")
+    .version("1.2.0")
     .description("A CLI to generate Next.js components with TypeScript support")
     .action(generateComponent);
 
